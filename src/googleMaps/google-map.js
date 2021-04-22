0dfd37985f172b34fa7676cf337946e1b627d4e8
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-04-22 13:51:44 +0000

    w-i-p

--- src/googleMaps/google-map.js
+++ src/googleMaps/google-map.js
@@ -4,7 +4,9 @@ import {
   LoadScript,
   Circle,
   Marker,
-  StreetViewService
+  StreetViewService,
+  InfoBox,
+  DistanceMatrixService
 } from "@react-google-maps/api";
 
 const containerStyle = {
@@ -36,13 +38,43 @@ const options = {
   zIndex: 1
 };
 
-const onLoad = (circle) => {
-  console.log("Circle onLoad circle: ", circle);
+const optionsInfo = { closeBoxURL: "", enableEventPropagation: true };
+
+const onLoad = (circle, infoBox, distanceMatrixService) => {
+  // console.log("Circle onLoad circle: ", circle);
+  //console.log("infoBox: ", infoBox);
+  console.log("distanceMatrixService: ", distanceMatrixService);
 };
 
-const onUnmount = (circle) => {
-  console.log("Circle onUnmount circle: ", circle);
+const onUnmount = (circle, distanceMatrixService) => {
+  // console.log("Circle onUnmount circle: ", circle);
+  console.log(
+    "DistanceMatrixService onUnmount distanceMatrixService: ",
+    distanceMatrixService
+  );
 };
+
+const origin1 = { lat: 55.93, lng: -3.118 };
+const origin2 = "Greenwich, England";
+const destinationA = "Stockholm, Sweden";
+const destinationB = { lat: 50.087, lng: 14.421 };
+
+const optionsDistance = {
+  origins: [origin1, origin2],
+  destinations: [destinationA, destinationB],
+  travelMode: google.maps.TravelMode.DRIVING,
+  unitSystem: google.maps.UnitSystem.METRIC,
+  avoidHighways: false,
+  avoidTolls: false
+};
+
+function callback(response, status) {
+  console.log(response, status);
+
+  response: DistanceMatrixResponse,
+  status: DistanceMatrixStatus
+
+}
 
 function MyComponent() {
   return (
@@ -50,7 +82,7 @@ function MyComponent() {
       <GoogleMap
         mapContainerStyle={containerStyle}
         center={center}
-        zoom={21}
+        zoom={20.5}
         mapTypeId="satellite"
       >
         {/* Child components, such as markers, info windows, etc. */}
@@ -65,10 +97,28 @@ function MyComponent() {
             // required
             options={options}
           />
-
           <Marker position={center} />
-
           <Marker position={userLocation} />
+          <InfoBox onLoad={onLoad} options={optionsInfo} position={center}>
+            <div
+              style={{
+                backgroundColor: "yellow",
+                opacity: 0.75,
+                padding: 12,
+                width: "auto"
+              }}
+            >
+              <div style={{ fontSize: 16, fontColor: `red` }}>
+                ICGC Elim Temple
+              </div>
+            </div>
+          </InfoBox>
+          <DistanceMatrixService
+            onLoad={onLoad}
+            onUnmount={onUnmount}
+            options={optionsDistance}
+            callback={callback}
+          ></DistanceMatrixService>
         </>
       </GoogleMap>
     </LoadScript>
