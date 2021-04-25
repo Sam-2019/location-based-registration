18aefe5fa74065520c85cc9773acc3517ca0814f
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-04-25 08:32:44 +0000

    added approutes

---
+++ src/Routes.js
@@ -0,0 +1,34 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  useRouteMatch,
+  Redirect
+} from "react-router-dom";
+import Signup from "./component/sign-up";
+import Register from "./component/register";
+import Map from "./Map";
+
+// import { useData } from "./Context";
+
+function AppROutes() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Map />
+        </Route>
+
+        <Route path="/signup">
+          <Signup />
+        </Route>
+
+        <Route path="/register">
+          <Register />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default AppROutes;
