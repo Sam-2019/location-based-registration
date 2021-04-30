22afe7180624e7bcf2197e92960be9b643f3ccc7
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-04-30 14:36:24 +0000

    renamed SVG, abstracted button & exTriangle

--- src/component/locationSVG.js
+++ src/component/locationSVG.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const SVG = () => {
+const LocationSVG = () => {
   return (
     <svg
       width="48px"
@@ -66,4 +66,4 @@ const SVG = () => {
   );
 };
 
-export default SVG;
+export default LocationSVG;
