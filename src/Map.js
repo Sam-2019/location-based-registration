dcbd13f10de34e769c85c3a38d476cfdd1c00355
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-05-01 21:48:57 +0000

    removed unused imports

--- src/Map.js
+++ src/Map.js
@@ -1,33 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import GoogleMap from "./googleMaps/new-maps";
-import useWatchLocation from "./hooks/useWatchLocation";
-import { geolocationOptions } from "./constants/geolocationOptions";
 
 const radius = 17.18;
 
 const Map = () => {
-  const { location, cancelLocationWatch, error } = useWatchLocation(
-    geolocationOptions
-  );
-  const [isWatchinForLocation, setIsWatchForLocation] = useState(true);
-
-  useEffect(() => {
-    if (!location) return;
-
-    // Cancel location watch after 3sec
-    setTimeout(() => {
-      cancelLocationWatch();
-      setIsWatchForLocation(false);
-    }, 3000);
-  }, [location, cancelLocationWatch]);
-
-  //console.log(isWatchinForLocation.toString());
-
-  return (
-    <>
-      <GoogleMap radius={radius} isWatchinForLocation={isWatchinForLocation} />
-    </>
-  );
+  return <GoogleMap radius={radius} />;
 };
 
 export default Map;
