11fb77069ce4aa4668fb75faa4c74d807f855c12
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-05-05 11:11:22 +0000

    removed unused code

--- src/index.js
+++ src/index.js
@@ -1,29 +1,11 @@
-import { StrictMode } from "react";
 import ReactDOM from "react-dom";
-import {
-  ApolloClient,
-  InMemoryCache,
-  gql,
-  ApolloProvider
-} from "@apollo/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import App from "./App";
 
 const client = new ApolloClient({
   uri: "http://localhost:5000/graphql",
   cache: new InMemoryCache()
 });
-
-// client
-//   .query({
-//     query: gql`
-//       {
-//         users {
-//           id
-//         }
-//       }
-//     `
-//   })
-//   .then((result) => console.log(result));
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
