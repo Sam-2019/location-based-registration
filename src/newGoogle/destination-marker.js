836611c7fe7bbe9c0ce6146ddc59d900d107122d
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-05-05 13:39:04 +0000

    updated destination-marker

--- src/newGoogle/destination-marker.js
+++ src/newGoogle/destination-marker.js
@@ -1,42 +1,33 @@
 import React from "react";
 import { Marker, Circle } from "react-google-maps";
 
-const BLACK_MARKER = "https://i.imgur.com/8dOrls4.png?2";
-const GREEN_MARKER = "https://i.imgur.com/9v6uW8U.png";
-
-function UserMarker() {
+function DestinationMarker() {
   const regionOptions = {
     fillOpacity: 0.1,
     strokeWidth: 1,
     strokeOpacity: 0.2
   };
 
-  const center = {
+  const destination = {
     lat: 5.755071140968645,
     lng: 0.05037
   };
 
-  const userLocation = {
-    lat: 5.755128,
-    lng: 0.050256
-  };
+  const radius = 17.18;
 
   const MARKER_SIZE = new google.maps.Size(50, 70);
   const MARKER_ICON = "https://i.imgur.com/Rhv5xQh.png";
 
-  const MARKER_SIZE = new google.maps.Size(25, 35);
-  const MARKER_ICON = within ? GREEN_MARKER : BLACK_MARKER;
-
   return (
     <>
       <Marker
-        position={userLocation}
-        title="You"
+        position={destination}
+        title="Destination"
         options={{ icon: { url: MARKER_ICON, scaledSize: MARKER_SIZE } }}
       />
-      <Circle center={center} radius={17.18} options={regionOptions} />
+      <Circle center={destination} radius={radius} options={regionOptions} />
     </>
   );
 }
 
-export default UserMarker;
+export default DestinationMarker;
