55916b773c820b8aa5d044080f648f9d15b36041
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-05-05 10:40:51 +0000

    removed loop

--- src/component/Success.js
+++ src/component/Success.js
@@ -30,7 +30,7 @@ export default Success;
 export function Animation() {
   return (
     <Lottie
-      loop
+      loop={false}
       animationData={lottieJson}
       play
       style={{ width: 250, height: 250 }}
