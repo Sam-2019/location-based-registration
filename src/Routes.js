b1c3c390fcbcf9f9b3fc821d332837a25d138038
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-05-07 00:15:12 +0000

    code update

--- src/Routes.js
+++ src/Routes.js
@@ -1,24 +1,31 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
 import Signup from "./component/sign-up";
 import Home from "./component/Home";
 import NotFound from "./component/NotFound";
 import PrivateRoute from "./component/PrivateRoute";
 
 function AppROutes() {
+  const find = localStorage.getItem("signupTOKEN");
+
   return (
     <Router>
       <Switch>
-        <Route exact path="/">
-          <Signup />
-        </Route>
+        {find ? (
+          <Route exact path="/">
+            <Home />
+          </Route>
+        ) : (
+          <Route exact path="/signup">
+            <Signup />
+          </Route>
+        )}
 
-        <PrivateRoute exact path="/register">
-          <Home />
-        </PrivateRoute>
-
-        <Route>
-          <NotFound />
-        </Route>
+        {/* {!find && <Redirect to="/signup" />} */}
       </Switch>
     </Router>
   );
