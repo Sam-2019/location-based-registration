e3e42ce215989f73d406d24cef7fa507672739d2
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-05-08 15:59:59 +0000

    added dashboard

--- src/admin/dashboard.js
+++ src/admin/dashboard.js
@@ -1,9 +1,17 @@
 import React from "react";
+import styled from "styled-components";
+
+const Header = styled.div`
+  padding: 10px;
+  background: #f0ffff;
+  font-size: 25px;
+  border-bottom: 1px solid gray;
+`;
 
 export default function Dashboard() {
   return (
     <>
-      <div>Dashboard</div>
+      <Header>Dashboard</Header>
     </>
   );
 }
